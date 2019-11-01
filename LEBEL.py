ec4a7a47448a4d04a97bc74ce492b257ed4093a1
Alexandre <alexandrebader3@gmail.com>
2019-11-01 14:32:43 +0100

    Update LEBEL.py

--- LEBEL.py
+++ LEBEL.py
@@ -1,5 +1,5 @@
-# Ce programme est la v1.0 du projet LEBEL
-# fait le 19/10/19, par Alexandre BADER
+# Ce programme est la v2.0 du projet LEBEL 
+# fait le 20/10/19, par Alexandre BADER
 
 def apprendre(list1, list2):
     global N
@@ -57,31 +57,101 @@ def test(list1, list2, list3):
     print("\nFin du test ",p,"/",N,sep='')
     input("Appuiez sur entrée pour revenir au menu...")
 
+def controle(list1, list2):
+    global N
+    print("Bienvenue dans le mode contrôle\n")
+    fr,et = list1[:], list2[:]
+    p = 0
+    corrige = []
+    for i in range(N):
+        r = randint(0, len(fr)-1)
+        if randint(0, 1):
+            print(i+1,'/',N,' <',fr[r], sep='',end='> : ')
+            juste = et[r]
+            corrige.append(fr[r])
+        else:
+            print(i+1,'/',N,' <',et[r], sep='',end='> : ')
+            juste = fr[r]
+            corrige.append(et[r])
+        reponse = input()
+        if reponse == juste:
+            p += 1
+            del corrige[-1]
+        else:
+            corrige.append(juste)
+            if reponse == '':
+                corrige.append('<vide>')
+            else:
+                corrige.append(reponse)
+        del fr[r]
+        del et[r]
+    print("\nFin du contrôle ",int(p/N*20),'/',20,sep='')
+    if p != N:
+        reponse =  input("Voulez voir le corrigé ? (Y/n) : ")
+        if reponse == 'Y':
+            for i in range(int(len(corrige)/3)):
+                print("La traduction de ",corrige[i*3]," est <",corrige[i*3+1],"> (≠",corrige[i*3+2],")", sep='', end='')
+                input()
+            print()
+    if p == N or reponse == 'Y':
+        input("Appuiez sur entrée pour revenir au menu...")
+
+def fichier():
+    f = open("mot.old.txt", 'r', encoding='Utf-8')
+    langue = ['']*2
+    list1 = []
+    list2 = []
+    tout = []
+    read = f.readline()
+    langue[0], langue[1] = read[1:3], read[4:6]
+    read = f.readline()
+    while read != '\n':
+        read = read[0:len(read)-1]
+        tout.append(read)
+        read = f.readline()
+    for mot in tout:
+        temp2 = ''
+        c = 0
+        while mot[c] != '=':
+            temp2 += mot[c]
+            c += 1
+        list1.append(temp2)
+        temp2 = ''
+        c+=1
+        while c != len(mot):
+            temp2 += mot[c]
+            c += 1
+        list2.append(temp2)
+    f.close()
+    return list1, list2, langue, len(list1)
+
 #programme principal
 from random import randint
-motFR = ['bonjour', 'chat', 'aujourd\'hui', 'demain', 'chien']
-motEN = ['hello', 'cat', 'today', 'tomorrow', 'dog']
-langue = ['fr', 'en']
-N = len(motFR)
+motFR, motET, langue, N = fichier() 
 play = True
+print("Bienvenue dans <LE Boss En Langue> !\n")
 while play:
-    print("    MENU")
-    print("1. Apprendre")
-    print("2. Reviser")
-    print("3. Test")
-    print("4. Quitter")
+    print("\n####### MENU ######")
+    print("## 1. Apprendre  ##")
+    print("## 2. Reviser    ##")
+    print("## 3. Test       ##")
+    print("## 4. Contrôle   ##")
+    print("## 5. Quitter    ##")
+    print("###################")
     try :
         choix = int(input("Lancer le mode : "))
     except:
         choix = 0
-        input("Désoler je n'ai pas compris")
+        input("Désoler, je n'ai pas compris...")
     print()
     if choix == 1:
-        apprendre(motFR, motEN)
+        apprendre(motFR, motET)
     elif choix == 2:
-        reviser(motFR, motEN, langue)
+        reviser(motFR, motET, langue)
     elif choix == 3:
-        test(motFR, motEN, langue)
+        test(motFR, motET, langue)
     elif choix == 4:
+        controle(motFR, motET)
+    elif choix == 5:
         play = 0
 print("\nA bientôt !")
